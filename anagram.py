8dedba1e6de53c5ae79ef04cb283a129018e493b
stethoma <stethoma@redhat.com>
2023-05-23 18:19:30 +0200

    update to angram

--- anagram.py
+++ anagram.py
@@ -2,11 +2,16 @@ str1 = input().strip() # integral
 str2 = input().strip() #triangle
 
 flag = True
+string1 = sorted(list(str1.lower().replace(" ", "")))
+string2 = sorted(list(str2.lower().replace(" ", "")))
 
-for ch in str1:
-    if ch not in str2:
-        flag = False
-        break
+if len(string1) == len(string2):
+    for ch in str1:
+        if ch not in str2:
+            flag = False
+            break
+else:
+    flag = False
 
 if flag:
     print("Anagram")
