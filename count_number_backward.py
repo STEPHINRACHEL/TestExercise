436d961f4dfbe8e335cb8b9b67cd4d8568b7b457
stethoma <stethoma@redhat.com>
2023-06-08 21:27:23 +0200

    add method to print result

--- count_number_backward.py
+++ count_number_backward.py
@@ -5,13 +5,32 @@
 # or prints just the number if none of those cases are true
 
 
-for i in range(100, 0, -1):
-    if i % 5 == 0 and i % 3 == 0:
-        print("Testing")
-    elif i % 5 == 0:
-        print("Agile")
-    elif i % 3 == 0:
+# for i in range(100, 0, -1):
+#     if i % 5 == 0 and i % 3 == 0:
+#         print("Testing")
+#     elif i % 5 == 0:
+#         print("Agile")
+#     elif i % 3 == 0:
+#         print("Software")
+#     else:
+#         print(i)
+#
+
+def print_result(num):
+    if isMultiple(num, 5):
+        if isMultiple(num, 3):
+            print("Testing")
+        else:
+            print("Agile")
+    elif isMultiple(num, 3):
         print("Software")
     else:
-        print(i)
+        print(num)
 
+
+def isMultiple(value, mod):
+    return value % mod == 0
+
+
+for i in range(100, 0, -1):
+    print_result(i)
